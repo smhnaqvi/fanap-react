969f3e2336e787205be716b20d064ef6000b0a82
smhnaqvi111 <smhanqvi111@gmail.com>
2020-11-08 19:21:10 +0330

    manage rendering in react +Display component +Updater component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,52 @@
-import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import DisplayUser from "./components/DisplayUser";
-import RegisterUser from "./components/RegisterUser";
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
 
+const Context = React.createContext();
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        color: theme.palette.text.secondary,
-    },
-}));
+function CountProvider({ value, children }) {
+  const [count, setCount] = useState(value);
+  return (
+    <Context.Provider value={{ count, setCount }}>{children}</Context.Provider>
+  );
+}
+
+function useCount() {
+  const context = React.useContext(Context);
+  if (context === undefined) {
+    throw new Error("useCount must be used");
+  }
+
+  const increment = () => setCount((c) => c + 1);
+
+  const { count, setCount } = context;
+  return { count, increment, setCount };
+}
+
+function Display() {
+  const { count } = useCount();
+  return (<h2>count: {count}</h2>);
+}
+
+function Updater() {
+  const { increment } = useCount();
+
+  const handelClick = () => {
+    increment();
+  };
+
+  return (<button onClick={handelClick}>Increment</button>);
+}
 
 export default function App() {
-    const classes = useStyles();
-    const [state, setState] = useState({
-        avatar: null,
-        firstName: null,
-        lastName: null,
-        phone: null,
-        email: null,
-        birthDate: null,
-        title: null,
-        isEmailVisible: false,
-        gender: null,
-        address: null
-    })
-
-
-    const handleRegisterForm = (data) => {
-        setState({
-            avatar: data.avatar,
-            firstName: data.firstName,
-            lastName: data.lastName,
-            phone: data.phone,
-            email: data.email,
-            birthDate: data.birthDate,
-            title: data.title,
-            isEmailVisible: data.isEmailVisible,
-            gender: data.gender,
-            address: data.address
-        })
-    }
-
-    return (
-        <React.Fragment>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Paper className={classes.paper}>
-                        <DisplayUser data={state}/>
-                    </Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper className={classes.paper}>
-                        <RegisterUser data={state} onSubmit={handleRegisterForm}/>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Container maxwidth="md">
+      <h3>COUNTER APP</h3>
+        <CountProvider value={10}>
+          <Display />
+          <Updater />
+        </CountProvider>
+      </Container>
+    </React.Fragment>
+  );
 }
